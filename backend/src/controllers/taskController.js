0db2ccf961519fe7aabc2c4445a0b7fe1b14406e
M Nabil Haritshah Away <82019717+Away1A@users.noreply.github.com>
2024-06-10 19:59:27 +0700

    cors

--- backend/src/controllers/taskController.js
+++ backend/src/controllers/taskController.js
@@ -98,3 +98,16 @@ exports.update = async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 };
+
+// Get specific task
+exports.get = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    if (!task) {
+      return res.status(404).json({ message: 'Task not found' });
+    }
+    res.status(200).json(task);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
