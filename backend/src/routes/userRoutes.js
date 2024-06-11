928e64e4ef741159b53dd4a76fc717879b5bd375
M Nabil Haritshah Away <82019717+Away1A@users.noreply.github.com>
2024-06-11 22:12:48 +0700

    Update userRoutes.js

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const auth = require('../middleware/authMiddleware');
 
-router.get('/', auth(['admin']), userController.index);
+router.get('/', auth(['admin', 'content_planner']), userController.index);
 router.get('/:id', auth(['admin']), userController.get);
 router.post('/', auth(['admin']), userController.create);
 router.put('/:id', auth(['admin']), userController.update);
