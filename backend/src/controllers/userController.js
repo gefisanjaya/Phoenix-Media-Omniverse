19c847cd7b11886914f181235a743980e2af21ed
M Nabil Haritshah Away <82019717+Away1A@users.noreply.github.com>
2024-06-12 19:39:26 +0700

    delete

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -66,12 +66,11 @@ exports.update = async (req, res) => {
 // Delete user
 exports.delete = async (req, res) => {
   try {
-    const user = await User.findById(req.params.id);
+    const user = await User.findByIdAndDelete(req.params.id);
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
     }
 
-    await user.remove();
     res.status(200).json({ message: 'User deleted' });
   } catch (err) {
     res.status(500).json({ message: err.message });
